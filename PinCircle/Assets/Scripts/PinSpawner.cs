239cd678e085b3fd64fdedd62dbc709538c963a0
LuBly <lumenize@ajou.ac.kr>
2023-04-10 20:16:26 +0900

    add PinSpawner

---
+++ PinCircle/Assets/Scripts/PinSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinSpawner : MonoBehaviour
+{
+    [Header("Commons")]
+    [SerializeField]
+    private GameObject pinPrefab; //»ç¿ëÇÒ ÇÉ ÇÁ¸®ÆÕ
+
+    public void SpawnThrowablePin(Vector3 position)
+    {
+        //ÇÉ ¿ÀºêÁ§Æ® »ý¼º
+        Instantiate(pinPrefab, position, Quaternion.identity);
+    }
+}
